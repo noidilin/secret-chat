535fc041431673c87e11f973ee29857a2053ce42
noidilin <linganinja.0120@gmail.com>
2026-02-04 03:06:40 +0800

    feat(cmp): wrap realtime provider around tanstack query

--- src/components/base/realtime-provider.tsx
+++ src/components/base/realtime-provider.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RealtimeProvider } from '@upstash/realtime/client'
 import { useState } from 'react'
 
 export default function RealtimeProviders({
@@ -12,6 +13,8 @@ export default function RealtimeProviders({
   const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <RealtimeProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </RealtimeProvider>
   )
 }
