14c1620b2fa66c197aa0036a768c6b290e80fb7e
noidilin <linganinja.0120@gmail.com>
2026-02-03 22:27:04 +0800

    feat(hook): generate animal name as user name

---
+++ src/hooks/use-username.ts
@@ -0,0 +1,30 @@
+import { nanoid } from 'nanoid'
+import { useEffect, useState } from 'react'
+
+const ANIMALS = ['wolf', 'hawk', 'bear', 'shark']
+const STORAGE_KEY = 'chat_username'
+
+const generateUsername = () => {
+  const word = ANIMALS[Math.floor(Math.random() * ANIMALS.length)]
+  return `anonymous-${word}-${nanoid(5)}`
+}
+
+export const useUsername = () => {
+  const [username, setUsername] = useState('')
+
+  // NOTE: make sure user retrieve the same name
+  useEffect(() => {
+    const stored = localStorage.getItem(STORAGE_KEY)
+
+    if (stored) {
+      setUsername(stored)
+      return
+    }
+
+    const generated = generateUsername()
+    localStorage.setItem(STORAGE_KEY, generated)
+    setUsername(generated)
+  }, [])
+
+  return { username }
+}
