320dd85c1d1d65ae9872a913dbf156f5fc3414a0
noidilin <linganinja.0120@gmail.com>
2026-02-04 12:00:50 +0800

    feat(server): add route middleware to handle auth for every route

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,6 +1,8 @@
 import { Elysia } from 'elysia'
 import { nanoid } from 'nanoid'
+import { z } from 'zod'
 import { redis } from '@/lib/redis'
+import { authMiddleware } from './auth'
 
 // time to live 10 minutes
 const ROOM_TTL_SECONDS = 60 * 10
@@ -19,6 +21,31 @@ const rooms = new Elysia({ prefix: '/room' })
 
     return { roomId }
   })
+  .use(authMiddleware) // define in auth.ts to act as a middleware
+
+const messages = new Elysia({ prefix: '/messages' })
+  // NOTE: since the middleware is executed before other method
+  // the following method can access the return object from middleware
+  .use(authMiddleware)
+  .post(
+    '/',
+    async ({ body, auth }) => {
+      const { sender, text } = body
+      const { roomId } = auth
+
+      const roomExists = await redis.exists(`meta:${roomId}`)
+      if (!roomExists) throw new Error('Room does not exist')
+    },
+    {
+      // PERF: define dedicated schema to validate every API request involves this data
+      // the post handler function only executed if the data's shape pass the validator
+      query: z.object({ roomId: z.string() }),
+      body: z.object({
+        sender: z.string().max(100),
+        text: z.string().max(1000),
+      }),
+    },
+  )
 
 // NOTE: app is the main router and it use rooms + messages
 export const app = new Elysia({ prefix: '/api' }).use(rooms).use(messages)
